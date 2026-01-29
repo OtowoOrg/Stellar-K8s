3b105c59e0bfc01c70a0acc616e6251ad05703d2
Oshioke-Salaki <salaki1902@gmail.com>
2026-01-29 14:34:40 +0100

    chore: fmt

--- src/crd/stellar_node.rs
+++ src/crd/stellar_node.rs
@@ -11,9 +11,9 @@ use serde::{Deserialize, Serialize};
 use super::types::{
     AutoscalingConfig, Condition, CrossClusterConfig, DisasterRecoveryConfig,
     DisasterRecoveryStatus, ExternalDatabaseConfig, GlobalDiscoveryConfig, HistoryMode,
-    HorizonConfig, IngressConfig, LoadBalancerConfig, NetworkPolicyConfig, NodeType, ResourceRequirements,
-    RetentionPolicy, RolloutStrategy, SorobanConfig, StellarNetwork, StorageConfig,
-    ValidatorConfig,
+    HorizonConfig, IngressConfig, LoadBalancerConfig, NetworkPolicyConfig, NodeType,
+    ResourceRequirements, RetentionPolicy, RolloutStrategy, SorobanConfig, StellarNetwork,
+    StorageConfig, ValidatorConfig,
 };
 
 /// Structured validation error for `StellarNodeSpec`
